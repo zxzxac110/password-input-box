b2c7d2b220199bacaa16751b2f09a5b309e19bf4
lishihao <lishihao@example.com>
2020-11-06 10:56:34 +0800

    打包配置

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vpay from 'vpay'
+import vpay from './lib/vpay'
 Vue.config.productionTip = false
 
 Vue.use(vpay)
